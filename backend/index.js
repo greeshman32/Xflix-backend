f3ffcbb4d33b59b94e6291d09198128691316f1f
B Greeshman <billagreeshman@gmail.com>
2022-12-04 21:55:12 +0530

    small bug

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const app = require("./app");
 
 let server=config.port;
 
-mongoose.connect("mongodb+srv://Gourav:<Greeshman97010>@cluster0.yaoxdnm.mongodb.net/xflix");
+mongoose.connect("mongodb+srv://Gourav:Greeshman97010@cluster0.yaoxdnm.mongodb.net/Xflix",
+                { useNewUrlParser: true },
+                ()=>{console.log("connected to mongodb")},
+                e=>{console.error(e)});
                 
 app.listen(server,()=>console.log("Listening at port ",server));
