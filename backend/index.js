81b6a3afa7694a816089690fda0eaffaa611aeaf
B Greeshman <billagreeshman@gmail.com>
2022-12-04 21:44:43 +0530

    small changes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,6 @@ const app = require("./app");
 
 let server=config.port;
 
-mongoose.connect("mongodb//127.0.0.1:24047");
+mongoose.connect("mongodb+srv://Gourav:<Greeshman97010>@cluster0.yaoxdnm.mongodb.net/xflix");
                 
 app.listen(server,()=>console.log("Listening at port ",server));
